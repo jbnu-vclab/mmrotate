d9902fa2a2a9818a87ddb247cccb3e8067419687
dongwon <ehddnjs@jbnu.ac.kr>
2024-04-09 00:28:38 +0900

    feat: update angle loss adaptive lambda

--- mmrotate/models/losses/angle_loss.py
+++ mmrotate/models/losses/angle_loss.py
@@ -5,6 +5,30 @@ from mmrotate.models.builder import ROTATED_LOSSES
 from mmrotate.core import obb2poly
 from mmdet.models.losses.utils import weighted_loss
 
+def get_up_vec_from_obb(obb):
+    poly = obb2poly(obb, version='le90')
+
+    upper_seg_mid_x = (poly[:, 0] + poly[:, 2]) / 2
+    upper_seg_mid_y = (poly[:, 1] + poly[:, 3]) / 2
+    upper_seg_mid = torch.stack((upper_seg_mid_x, upper_seg_mid_y), dim=1)
+
+    direction = upper_seg_mid - obb[:, :2]
+    direction = direction / torch.norm(direction, dim=1, keepdim=True)
+
+    return direction
+
+def get_cos_sim_loss(pred, target):
+    direction_p = get_up_vec_from_obb(pred)
+    direction_t = get_up_vec_from_obb(target)
+
+    cos = nn.CosineSimilarity(dim=1)
+
+    cosine_similarity = cos(direction_p, direction_t)
+
+    cos_sim_loss = 1 - cosine_similarity
+
+    return cos_sim_loss
+
 @weighted_loss
 def angle_loss(pred, target, beta=1.0, cos_weight=1.0):
     """Angle loss.
@@ -30,32 +54,9 @@ def angle_loss(pred, target, beta=1.0, cos_weight=1.0):
     xy_loss = torch.where(diff < beta, 0.5 * diff * diff / beta,
                           diff - 0.5 * beta).sum(dim=-1)
     
-    # Angle norm (cosine similarity between two direction vectors)
-    poly_p = obb2poly(pred, version='le90')
-    poly_t = obb2poly(target, version='le90')
-
-    upper_seg_mid_p_x = (poly_p[:, 0] + poly_p[:, 2]) / 2
-    upper_seg_mid_p_y = (poly_p[:, 1] + poly_p[:, 3]) / 2
-    upper_seg_mid_t_x = (poly_t[:, 0] + poly_t[:, 2]) / 2
-    upper_seg_mid_t_y = (poly_t[:, 1] + poly_t[:, 3]) / 2
-
-    upper_seg_mid_p = torch.stack((upper_seg_mid_p_x, 
-                                                upper_seg_mid_p_y), 
-                                                dim=1)
-    upper_seg_mid_t = torch.stack((upper_seg_mid_t_x, 
-                                                upper_seg_mid_t_y), 
-                                                dim=1)
+    adaptive_lambda = torch.tensor([0.5, 0.5, 0.5, 0.5, 1.0], dtype=torch.float32)
     
-    direction_p = upper_seg_mid_p - xy_p
-    direction_t = upper_seg_mid_t - xy_t
-
-    cos = nn.CosineSimilarity(dim=1)
-    
-    cosine_similarity = cos(direction_p, direction_t)
-    
-    cos_sim_loss = 1 - cosine_similarity
-
-    loss = xy_loss + cos_sim_loss * cos_weight
+    loss = xy_loss * adaptive_lambda
     
     return loss
 
